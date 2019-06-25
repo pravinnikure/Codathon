44d534d31b8eb1e05601bfbb009e733002bb9ac2
Akash Gudadhe <akashgudadhe112@gmail.com>
2019-06-25 22:36:17 +0530

    Enormous Input Test

---
+++ codechef/INTEST.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{       
+  int n, k;
+
+  int count=0;
+
+  cin>>n>>k;
+  int arr[n];
+
+  for(int i=0; i<n; i++)
+  {
+     cin>>arr[i];
+  }
+
+  for(int i=0; i<n; i++)
+  {
+    if(arr[i]%k == 0)
+    {
+      count++;
+    }
+  }
+  
+
+  cout<<count<<endl;
+
+
+  return 0;
+} 
