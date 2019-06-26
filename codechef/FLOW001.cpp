09b9394d5c3ca1a1e68f35f2f7a6128d0418a837
Akash Gudadhe <akashgudadhe112@gmail.com>
2019-06-26 22:26:12 +0530

    Add two numbers in matrix

---
+++ codechef/FLOW001.cpp
@@ -0,0 +1,36 @@
+/* https://www.codechef.com/problems/FLOW001 */
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  int n;
+  cin>>n;
+
+  int in_arr[n][2];
+
+  for(int i=0; i<n; i++)
+  {
+    for(int j=0; j<2; j++)
+    {   
+      cin>>in_arr[i][j];
+    }   
+  }
+
+
+  for(int i=0; i<n; i++)
+  {
+    for(int j=0; j<1; j++)
+    {   
+      int sum = 0;    
+      sum = sum + in_arr[i][0] + in_arr[i][1];
+      cout<<sum<<endl;
+    }   
+  }
+
+
+  return 0;
+}
+
