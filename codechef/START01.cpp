98667d9578c2e3f43a8d320b57d9b807df800460
Akash Gudadhe <akashgudadhe112@gmail.com>
2019-06-27 20:42:00 +0530

    Number Mirror

---
+++ codechef/START01.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/submit/START01
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    
+    cin>>num;
+    cout<<num<<endl;
+    
+    return 0;
+}
