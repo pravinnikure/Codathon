df09bd3913feaa97196c6365592a8daaac1c9caa
Akash Gudadhe <akashgudadhe112@gmail.com>
2019-06-27 23:50:23 +0530

    Life, the Universe and Everything

---
+++ codechef/TEST.cpp
@@ -0,0 +1,27 @@
+/*
+  https://www.codechef.com/submit/TEST
+*/
+
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    
+    while(1)
+    {
+        cin>>num;
+        
+        if(num == 42)
+        {
+            break;
+        }else{
+            cout<<num<<endl;
+        }
+    }
+
+    return 0;
+}
