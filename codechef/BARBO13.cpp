31f78f1f1a487648ea7086a4f470b42ae337a8a8
Akash Gudadhe <akashgudadhe112@gmail.com>
2019-07-07 16:47:48 +0530

    ASCII Value

---
+++ codechef/BARBO13.cpp
@@ -0,0 +1,22 @@
+/*
+  https://www.codechef.com/problems/BARBO13
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int T;
+   cin>>T;
+   
+   while(T--)
+   {
+       char ch;
+       cin>>ch;
+       
+       cout<<(int)ch<<endl;
+   }
+}
+
+
