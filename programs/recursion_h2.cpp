2508dc15b4825fee0604bb176908ee178420a88c
Akash Gudadhe <akashgudadhe112@gmail.com>
2019-07-30 19:12:23 +0530

    Factorial using recursion

---
+++ programs/recursion_h2.cpp
@@ -0,0 +1,23 @@
+//Calculating factorial using recursion
+#include<iostream>
+
+using namespace std;
+
+int fact(int n)
+{
+  if(n == 1){
+    return 1;
+  }else{
+    return n * fact(n-1);
+  }
+}
+
+int main()
+{
+  int n;	
+  cin>>n;
+
+  cout<<fact(n);
+
+  return 0;	 
+}
