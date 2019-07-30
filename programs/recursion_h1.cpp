9c11566d5dc029e334212b3b095a18a4cc810681
Akash Gudadhe <akashgudadhe112@gmail.com>
2019-07-30 18:56:16 +0530

    Recursion Homework

---
+++ programs/recursion_h1.cpp
@@ -0,0 +1,20 @@
+//Recursion Demo
+#include<iostream>
+
+using namespace std;
+
+int fun(int n)
+{
+  if(n==0){
+    return 1;
+  }else{
+    return 7 + fun(n-2);
+  }
+}
+
+int main()
+{
+  printf("%d", fun(4));
+  	
+  return 0;	
+}
